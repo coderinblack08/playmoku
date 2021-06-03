5345de993a9122ce7cad8d9732a3c1e68126e894
coderinblack <kevinlu.email@gmail.com>
2021-06-03 15:06:27 -0700

    feat(web): Add firebase realtime database and auth emuatlors

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,12 @@
 import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
 import { config } from "./firebaseConfig";
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
+  if (process.env.NODE_ENV === "development") {
+    firebase.auth().useEmulator("http://localhost:9099");
+    firebase.database().useEmulator("localhost", 9000);
+  }
 }
